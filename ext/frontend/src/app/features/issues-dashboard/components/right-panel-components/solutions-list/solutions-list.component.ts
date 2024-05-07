05257c23385bc6deeae15e56c7ea9ec67d49796b
MRadzynski <radzynski.milosz@gmail.com>
2024-05-08 00:41:11 +0200

    FE: send metrics request after copying the sources link

--- ext/frontend/src/app/features/issues-dashboard/components/right-panel-components/solutions-list/solutions-list.component.ts
+++ ext/frontend/src/app/features/issues-dashboard/components/right-panel-components/solutions-list/solutions-list.component.ts
@@ -1,32 +1,37 @@
 import { Clipboard } from '@angular/cdk/clipboard';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { TranslateService } from '@ngx-translate/core';
 import {
-  ReportIssueAnalysisComponent
-} from 'app/features/issues-dashboard/components/right-panel-components/report-issue-analysis/report-issue-analysis.component';
-import { DetailedIssueDTO, DetailedIssueScore } from 'app/shared/interfaces/DetailedIssueDTO';
+  DetailedIssueDTO,
+  DetailedIssueScore,
+} from 'app/shared/interfaces/DetailedIssueDTO';
+import { MatDialog } from '@angular/material/dialog';
+import { MetricsService } from 'app/shared/services/metrics.service';
+import { ReportIssueAnalysisComponent } from 'app/features/issues-dashboard/components/right-panel-components/report-issue-analysis/report-issue-analysis.component';
+import { TranslateService } from '@ngx-translate/core';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-solutions-list',
   templateUrl: './solutions-list.component.html',
-  styleUrls: [ './solutions-list.component.scss' ]
+  styleUrls: ['./solutions-list.component.scss'],
 })
 export class SolutionsListComponent {
   @Input()
   public activeIssue: DetailedIssueDTO;
   @Output()
-  public scoreSelected: EventEmitter<DetailedIssueScore> = new EventEmitter<DetailedIssueScore>();
+  public scoreSelected: EventEmitter<DetailedIssueScore> =
+    new EventEmitter<DetailedIssueScore>();
   @Output()
   public markIssueAsResolved: EventEmitter<void> = new EventEmitter<void>();
   @Output()
   public regenerateIssue: EventEmitter<void> = new EventEmitter<void>();
-  constructor(private clipboard: Clipboard,
-              private toastrService: ToastrService,
-              private translateService: TranslateService,
-              private dialog: MatDialog) {
-  }
+  constructor(
+    private clipboard: Clipboard,
+    private toastrService: ToastrService,
+    private translateService: TranslateService,
+    private dialog: MatDialog,
+    private metricsService: MetricsService
+  ) {}
 
   public positiveScoreSelected(): void {
     if (this.activeIssue.score === 1) {
@@ -48,15 +53,18 @@ export class SolutionsListComponent {
 
   public copyLink(link: string): void {
     this.clipboard.copy(link);
-    this.toastrService.success(this.translateService.instant('FEATURES.ISSUES.LINK_COPIED_TO_CLIPBOARD'));
+    this.toastrService.success(
+      this.translateService.instant('FEATURES.ISSUES.LINK_COPIED_TO_CLIPBOARD')
+    );
+    this.metricsService.copySourcesLinkClick(this.activeIssue.id).subscribe();
   }
 
   public openReportIssueAnalysisModal(): void {
     this.dialog.open(ReportIssueAnalysisComponent, {
       width: '500px',
       data: {
-        issueId: this.activeIssue.id
-      }
+        issueId: this.activeIssue.id,
+      },
     });
   }
 }
